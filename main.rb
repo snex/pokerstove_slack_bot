af1850b72f883b3773c9478d09b07cf8f9847123
dblock <dblock@dblock.org>
2019-03-18 16:59:50 -0400

    Allow message loops, fixes https://github.com/slack-ruby/slack-ruby-bot/issues/215.

--- main.rb
+++ main.rb
@@ -1,3 +1,7 @@
 require_relative 'poker_stove_bot'
 
+SlackRubyBot.configure do |config|
+  config.allow_message_loops = true
+end
+
 PokerStoveBot.run
--- spec/poker_stove_bot_spec.rb
+++ spec/poker_stove_bot_spec.rb
@@ -2,5 +2,16 @@ require_relative 'spec_helper'
 require_relative '../poker_stove_bot'
 
 RSpec.describe PokerStoveBot do
+  subject do
+    PokerStoveBot.instance
+  end
+
   it_behaves_like 'a slack ruby bot'
+
+  it 'works' do
+    expect(Process).to receive(:spawn).and_wrap_original do |m, cmd, *args|
+      m.call('echo', *args)
+    end
+    expect(message: '!ps hello world').to respond_with_slack_message("hello world\n")
+  end
 end
